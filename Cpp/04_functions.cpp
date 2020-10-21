45c68c64ef953dd454742e34d004a9e2f99af0ed
Viktor Kreschenski <viktor.kreschenski@altran.com>
2020-10-21 12:48:56 +0200

    Added function usage comparison

---
+++ Cpp/04_functions.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+
+// ####################################################################################
+// # Simple function definiton
+void say_hello(){
+    std::cout << "Hello world!" << std::endl;
+}
+
+void say_hello(std::string& name){
+    std::cout << "Hello " << name << std::endl;
+}
+
+auto mean_manual(const std::vector<int>& seq){
+    auto n = 0.0;
+    for(auto x: seq)
+        n += x;
+    return n/seq.size();
+}
+
+// ####################################################################################
+// Call by value/reference
+auto mean_value(const std::vector<int> seq){
+    // Two copies
+    auto n = 0.0;
+    for(auto x: seq)
+        n += x;
+    return n/seq.size();
+}
+
+void mean_reference(const std::vector<int>& seq, float& mean_val){
+    // Zero copies
+    auto n = 0.0;
+    for(auto x: seq)
+        n += x;
+    mean_val = n/seq.size();
+}
+
+// ####################################################################################
+// # Define a function pointer
+auto(*other_mean_name)(const std::vector<int>& seq) = mean_manual;
+
+void ForEach(const std::vector<int>& values, void(*func)(int)){
+    for(int value: values){
+        func(value);
+    }
+}
+
+// ####################################################################################
+// # Templates in python are implicit and builtin
+template <typename T>
+auto mean_template(const std::vector<T>& seq){
+    auto n = 0.0;
+    for(auto x: seq)
+        n += x;
+    return n/seq.size();
+}
+
+// ####################################################################################
+// # Return multiple values
+struct Ints {
+    int a, b;
+};
+
+Ints multiple_values(const int& a, const int& b){
+    Ints ints;
+    ints.a = a;
+    ints.b = b;    
+    return ints;
+}
+
+// ####################################################################################
+// # Lambda functions
+auto mean_lambda = [](const std::vector<int>& seq){
+    return reduce(par_unseq, begin(seq), end(seq))/seq.size();
+};
+
+// ####################################################################################
+// # main function
+int main(){
+    std::vector<double> rvec = {1, 2 ,3, 4 ,5};
+    std::cout << mean_template(rvec) << std::endl;
+
+    Ints intret = multiple_values(1, 2);
+    std::cout << intret.a << ", " << intret.b << std::endl;
+    return 0;
+}
--- Py/04_functions.py
+++ Py/04_functions.py
@@ -3,22 +3,55 @@
 def say_hello():
     print("Hello world!")
 
-def say_hello(name):
+# Explicit type definition
+def say_hello(name: str) -> None:
     print(f"Hello {name}!")
+
+def mean_manual(seq):
+    n = 0.0
+    for x in seq:
+        n += x
+    return n/len(seq)
+
+def mean_builtin(seq):
+    return sum(seq)/len(seq)
 
 # Prototyping is not needed
 
 ####################################################################################
-# Call by reference/value example in python
+# Call by reference/value example in python only with objects possible
 
 ####################################################################################
 # Pass function address and run
+other_mean_name = mean_manual
+
+def ForEach(values, func):
+    for value in values:
+        func(value)
+
 
 ####################################################################################
 # Python decorators
 
 ####################################################################################
 # Return multiple values
+def multiple_values(a, b):
+    return a, b
 
 ####################################################################################
 # Templates in python are implicit and builtin
+# Means calculation of float or int are already considered
+
+####################################################################################
+# Lambda functions
+mean = lambda seq: sum(seq) / len(seq)
+
+####################################################################################
+# main function
+def main():
+    rvec = [1, 2, 3, 4, 5]
+    print(mean_manual(rvec))
+    c, d = multiple_values(1, 2)
+
+if __name__=="__main__":
+    main()
